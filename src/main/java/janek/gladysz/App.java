b6dd669d4a1fa04980d2384d044d20bd0b2a29d6
JanuSzymu <janek.mail@gmail.com>
2022-09-30 18:11:41 +0200

    pierwsz projekt

---
+++ src/main/java/janek/gladysz/App.java
@@ -0,0 +1,31 @@
+package janek.gladysz;
+
+public class App {
+
+
+    public static void main(String[] args) {
+            printMessage("chomikowej");
+            printMessage("glorpo");
+
+    }
+
+    static void printMessage(String newschool) {
+
+
+        System.out.println("witajcie.chomikowie");
+        System.out.println("w nowyej szkole " + newschool);
+        System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
+
+        System.out.println("witajcie.chomikowie");
+        System.out.println("w nowyej szkole " + newschool);
+        System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
+    }
+}
+
+
+
+
+
+
+
+
