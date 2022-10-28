b8985a42eded2e9e6c1a9e98fcdc110d082bab62
JanuSzymu <janek.mail@gmail.com>
2022-10-28 18:07:45 +0200

    Variabels and methods

--- src/main/java/janek/gladysz/App.java
+++ src/main/java/janek/gladysz/App.java
@@ -1,33 +1,34 @@
 package janek.gladysz;
 
+import javax.xml.namespace.QName;
+
 public class App {
 
 
     public static void main(String[] args) {
 
-        String[] names = {"boft","mikjh","ghytr"};
+        String[] names = {"boft", "mikjh", "ghytr", "surykatka"};
 
 
-
-
-        for (int i = 0; i < names.length;   i++) {
-            printMessage(names[i]);
-            System.out.println(i);
-
-        }
-
+        for (String each : names)
+            printMessage(each);
     }
+
 
     static void printMessage(String newschool) {
 
+        if ("surykatka".equals(newschool)) {
+            System.out.println("to niestety ostatni rok");
+        }else{
+            System.out.println("witajcie.chomikowie");
+            System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
+            System.out.println("w nowyej szkole " + newschool);
+            System.out.println("witajcie.chomikowie");
+            System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
+            System.out.println("w nowyej szkole " + newschool);
 
-        System.out.println("witajcie.chomikowie");
-        System.out.println("w nowyej szkole " + newschool);
-        System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
+        }
 
-        System.out.println("witajcie.chomikowie");
-        System.out.println("w nowyej szkole " + newschool);
-        System.out.println("wszystko jest tu w rozmiarze pani " + newschool);
     }
 }
 
